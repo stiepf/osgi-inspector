c53aa562170fe4228d43be551fc46eaf42e6905a
Stefan Schulze <stiepf@googlemail.com>
2013-07-08 17:29:24 +0200

    license

--- osgi-inspector-bundles/src/main/java/com/github/stiepf/inspector/bundles/internal/BundleQueryImpl.java
+++ osgi-inspector-bundles/src/main/java/com/github/stiepf/inspector/bundles/internal/BundleQueryImpl.java
@@ -1,3 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.stiepf.inspector.bundles.internal;
 
 import java.util.Arrays;
@@ -14,19 +27,6 @@ import org.osgi.service.packageadmin.PackageAdmin;
 
 import com.github.stiepf.inspector.bundles.BundleQuery;
 
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 public class BundleQueryImpl implements BundleQuery {
 
   private final BundleContext bundleContext;
