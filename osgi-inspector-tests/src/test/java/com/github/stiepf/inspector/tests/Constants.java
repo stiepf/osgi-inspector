e129fbb981bef9c49e3627519d02805b152d9f7c
Stefan Schulze <stiepf@googlemail.com>
2013-07-31 14:27:15 +0200

    Maven coordinates as constants.

---
+++ osgi-inspector-tests/src/test/java/com/github/stiepf/inspector/tests/Constants.java
@@ -0,0 +1,14 @@
+package com.github.stiepf.inspector.tests;
+
+public final class Constants {
+
+  private Constants() {}
+ 
+  public static final String GROUP_ID = "com.github.stiepf.inspector";
+  public static final String BUNDLES_ARTEFACT_ID = "osgi-inspector-bundles";
+  public static final String SERVICES_ARTEFACT_ID = "osgi-inspector-services";
+  public static final String COMPONENTS_ARTEFACT_ID = "osgi-inspector-components";
+  public static final String CURRENT_VERSION = "0.1.1-SNAPSHOT";
+ 
+  
+}
